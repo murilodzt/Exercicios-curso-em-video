7a21e7ec1648e6633416464ba6665abebd301efb
murilodzt <murilodzt@gmail.com>
2026-03-27 22:35:15 -0300

    Exercicio 18

---
+++ ex 18.py
@@ -0,0 +1,6 @@
+import math
+a = float(input('digite o valor do angulo em graus: '))
+b = math.radians(a)
+print(f'O seno de {a} é {math.sin(b):.2f}')
+print(f'O cosseno de {a} é {math.cos(b):.2f}')
+print(f'A tangente de {a} é {math.tan(b):.2f}') 
